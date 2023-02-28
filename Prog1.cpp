fdb4e019949f635a372f81a77e5e3357dad1ea2c
PANKAJ SADHUKHAN <pankajsadhukhan243@gmail.com>
2023-02-28 20:49:29 +0530

    Create Prog1.cpp

---
+++ Prog1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class Myclass{
+    public:
+    int x;
+    private:
+    int y;
+};
+int main()
+{
+    Myclass obj1;
+    obj1.x=25;
+    //obj1.y=23;
+    return 0;
+}
