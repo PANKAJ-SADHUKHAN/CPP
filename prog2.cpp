aaec84f7e2f1bff6482373da5c1f80073f44043b
PANKAJ SADHUKHAN <pankajsadhukhan243@gmail.com>
2023-02-28 20:56:37 +0530

    Create prog2.cpp

---
+++ prog2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+class Myclass{
+public:
+int roll;
+string name;
+};
+int main(){
+Myclass obj1;
+obj1.name="Pankaj";
+obj2.roll=12;
+cout<<obj1.name<<endl;
+cout<<obj1.roll<<endl;
+return 0;
+}
